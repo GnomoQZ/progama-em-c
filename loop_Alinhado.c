df31a910d21b218ae97e96f969dcaadc44ffb98e
GnomoQZ <202402914677@alunos.estacio.br>
2025-02-27 22:21:20 +0000

    ll

---
+++ loop_Alinhado.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+    int main(){
+
+        //Exemplo em for
+        for (int i = 0; i <= 10; i++)
+        {   for (int j = 0; j <= 10; j++)
+        {
+            printf("%d X %d = %d", i, j, i*j);
+        }
+        
+
+            printf("\n");
+        }
+        
+
+
+
+
+        return 0;
+    }
