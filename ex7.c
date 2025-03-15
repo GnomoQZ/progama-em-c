d4eef660ce843099b50b3eec959a43621f2c953c
GnomoQZ <202402914677@alunos.estacio.br>
2025-03-15 18:00:45 +0000

    d

--- ex7.c
+++ ex7.c
@@ -1,9 +1,9 @@
-#include <stab.h>
+#include <stdio.h>
 // Vertor Matriz
 
 
 int main(){
-    int index;
+    /*int index;
 
     char *nomesAlunos [3][3]= {
         {"aluno0", "pt: 30", "mat: 90"},
@@ -19,10 +19,43 @@ int main(){
     scanf("%d", &index);
 
     printf("A nota do %s são: %s, %s \n", nomesAlunos[index][0], nomesAlunos[index][1], nomesAlunos[index][2] );
-
-    int matris [2][2] = {
-        {0},{1},
-        {2},{3}
-    };
-    return 0;
-}
+*/
+    // Declaração e inicialização da matriz
+  
+        // Declaração e inicialização da matriz 10x10 com zeros
+        int matris[10][10] = {
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,3,3,3,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+        };
+    
+        // Declaração do array de letras para rotular as linhas
+        char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    
+        // Exibição da matriz formatada como tabela
+        printf("   "); // Espaço para alinhar os rótulos das colunas
+        for (int i = 1; i < 11; i++) {
+            printf("%d ", i); // Imprime os números das colunas (0-9)
+        }
+        printf("\n");
+    
+        for (int i = 0; i < 10; i++) {
+            printf("%c  ", letras[i]); // Imprime a letra correspondente à linha
+    
+            for (int j = 0; j < 10; j++) {
+                printf("%d ", matris[i][j]); // Imprime os valores da matriz
+            }
+            printf("\n"); // Pula para a próxima linha
+        }
+    
+        return 0;
+    }
+    
